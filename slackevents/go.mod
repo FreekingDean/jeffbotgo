77cfaf64b39aae9297f2d44409541675ee009e40
Dean <deangalvin3@gmail.com>
2019-01-20 17:55:35 -0500

    Update vendz

--- slackevents/go.mod
+++ slackevents/go.mod
@@ -3,6 +3,9 @@ module slackevents
 require (
 	cloud.google.com/go v0.34.0
 	github.com/googleapis/gax-go v2.0.2+incompatible // indirect
+	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/nlopes/slack v0.5.0
+	github.com/pkg/errors v0.8.1 // indirect
 	golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4 // indirect
 	google.golang.org/api v0.1.0 // indirect
 	google.golang.org/genproto v0.0.0-20190111180523-db91494dd46c // indirect
--- slackevents/slackevents.go
+++ slackevents/slackevents.go
@@ -3,10 +3,12 @@ package slackevents
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"cloud.google.com/go/pubsub"
+	"github.com/nlopes/slack/slackevents"
 )
 
 func SlackEvent(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +24,28 @@ func SlackEvent(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(500)
 	}
 	body := buf.String()
+	eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body))
+	if e != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	if eventsAPIEvent.Type == slackevents.URLVerification {
+		var r *slackevents.ChallengeResponse
+		err := json.Unmarshal([]byte(body), &r)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		w.Header().Set("Content-Type", "text")
+		w.Write([]byte(r.Challenge))
+	}
+	if eventsAPIEvent.Type == slackevents.CallbackEvent {
+		innerEvent := eventsAPIEvent.InnerEvent
+		switch ev := innerEvent.Data.(type) {
+		case *slackevents.MessageEvent:
+			log.Println(ev.Text)
+			//api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
+		}
+	}
 	log.Println(body)
 	w.WriteHeader(200)
 }
