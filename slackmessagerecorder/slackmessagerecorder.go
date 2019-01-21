ee974022343268cc1695d4169dde6e48d1c3e5e8
Dean <deangalvin3@gmail.com>
2019-01-20 20:29:33 -0500

    Added slack message stuff

---
+++ slackmessagerecorder/slackmessagerecorder.go
@@ -0,0 +1,22 @@
+package slackmessagerecorder
+
+import (
+	"context"
+	"log"
+)
+
+// PubSubMessage is the payload of a Pub/Sub event. Please refer to the docs for
+// additional information regarding Pub/Sub events.
+type PubSubMessage struct {
+	Data []byte `json:"data"`
+}
+
+// HelloPubSub consumes a Pub/Sub message.
+func HelloPubSub(ctx context.Context, m PubSubMessage) error {
+	name := string(m.Data)
+	if name == "" {
+		name = "World"
+	}
+	log.Printf("Hello, %s!", name)
+	return nil
+}
